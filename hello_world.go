bd58b07a0f059fcb839a3519c4d09d98f42fdd2a
Mr.KLeo <59167582+lizhuang-zhi@users.noreply.github.com>
2023-06-16 22:01:34 +0800

    gin的入门：0.0.0.0讲解

---
+++ hello_world.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+
+)
+
+func Index(ctx *gin.Context) {
+	ctx.String(200, "Hello World! Leo is Good")
+}
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/index", Index)
+
+	// 启动监听：gin会把web服务运行在本机的0.0.0.0:8080端口上
+	router.Run(":8080")
+	// 用原生http服务的方式, router.Run本质就是的进一步封装（可以看源码）
+	http.ListenAndServe(":8080", router)
+}
