851f159a440c6c98b3533b7e6eaf868f0edfbcc1
Mr.KLeo <59167582+lizhuang-zhi@users.noreply.github.com>
2023-06-22 20:03:02 +0800

    gin的入门: 表单参数

--- 视图/2.请求.go
+++ 视图/2.请求.go
@@ -23,10 +23,25 @@ func _param(c *gin.Context) {
 	fmt.Println(c.Param("book_id")) // 123
 }
 
+func _form(c *gin.Context) {
+	/*
+		传递的form-data: name: leo li
+					    name: Tom Zhang
+						addr: 陕西省
+						file: test.png(一张图片)
+	*/
+	fmt.Println(c.PostForm("name"))               // leo li
+	fmt.Println(c.PostFormArray("name"))          // [leo li Tom Zhang]
+	fmt.Println(c.DefaultPostForm("addr", "四川省")) // 陕西省
+	forms, err := c.MultipartForm()  // 接收所有的参数, 包括文件
+	fmt.Println(forms, err) // &{map[addr:[陕西省] name:[leo li Tom Zhang]] map[file:[0xc0004ce000]]} <nil>
+}
+
 func main() {
 	router := gin.Default()
 	router.GET("/query", _query)
 	router.GET("/param/:user_id", _param)
 	router.GET("/param/:user_id/:book_id", _param)
+	router.POST("/form", _form)
 	router.Run(":80")
 }
