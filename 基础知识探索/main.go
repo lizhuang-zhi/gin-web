576826f1a152d10555584fb69df8cf232677fb54
Mr.KLeo <59167582+lizhuang-zhi@users.noreply.github.com>
2023-12-03 19:03:16 +0800

    再次区分理解结构体指针方法和结构体方法

--- 基础知识探索/main.go
+++ 基础知识探索/main.go
@@ -3,36 +3,34 @@ package main
 import "fmt"
 
 type User struct {
-    UserID int         `json:"user_id"`
     Name   string      `json:"name"`
-    Age    int         `json:"age"`
-    Wallet interface{} `json:"wallet"`
 }
 
-// 定义结构体的方法
-func (u User) SetNameValue(val string) {
-    u.Name = val
-}
+// // 定义结构体的方法
+// func (u User) SetNameValue(val string) {
+//     u.Name = val
+// }
 
 // 定义结构体指针的方法
-func (u *User) SetNamePointer(val string) {
+func (u *User) SetNameValue(val string) {
     u.Name = val
 }
 
 func main() {
-    user := User{
-        UserID: 1,
+    user1 := User{
         Name:   "Leo",
-        Age:    22,
-        Wallet: 10000,
     }
-    fmt.Println(user)  // {1 Leo 22 10000}
+	user2 := User{
+        Name:   "Jim",
+    }
+    fmt.Println(user1)  // {Leo}
+    fmt.Println(user2)  // {Jim}
 
-    user.SetNameValue("Jim")
-    // 由于是对值的拷贝, 所以不会将改变带出SetNameValue函数
-    fmt.Println(user)  // {1 Leo 22 10000} 
+    user1.SetNameValue("Jeo")
+    fmt.Println(user1)  // {Jeo}
+    fmt.Println(user2)  // {Jim}
 
-    user.SetNamePointer("Tim")
-    // 由于是对结构体地址的访问, 所以会将改变带出SetNamePointer函数
-    fmt.Println(user)  // {1 Tim 22 10000}
+    user2.SetNameValue("Tim")
+    fmt.Println(user1)  // {Jeo}
+    fmt.Println(user2)  // {Tim}
 }
