83762a9c51737b298fb382bd8aae81f233842ef8
Mr.KLeo <59167582+lizhuang-zhi@users.noreply.github.com>
2024-01-12 09:57:26 +0800

    复习并发代码-5
    计算1-8000的素数有哪些？

--- 并发/channel/复习代码.go
+++ 并发/channel/复习代码.go
@@ -5,42 +5,67 @@ import (
 )
 
 /*
-需求:
-1. 开启一个writeData协程, 向管道intChan中写入50个整数
-2. 开启一个readData协程, 从管道intChan中读取writeData写入的数据[注意这里可能是边写边读]
-3. 注意: writeData和readData操作的是同一个管道
-4. 主线程需要等待writeData和readData协程都完成工作才能退出管道
+	计算1-8000的素数有哪些？（使用gorountine）
+	1. 使用intChan存储1-8000的素数
+	2. 开启4个协程, 从intChan中读取数据，然后判断是否为素数，然后将素数存入primeChan中
+	3. 每个协程执行完毕，则往exitChan中存入一个true
+	4. 主线程中循环等待exitChan全部取出后退出
+	5. 遍历primeChan中的素数
 */
 
-var intChan chan int = make(chan int, 5)
-var exitChan chan bool = make(chan bool, 1)
-
 func main() {
-	go writeData()
-	go readData()
+	var intChan chan int = make(chan int, 1000)
+	var primeChan chan int = make(chan int, 3000)
+	var exitChan chan bool = make(chan bool, 4)
+
+	go inputNum(intChan)
+
+	for i := 0; i < cap(exitChan); i++ {
+		go judgePrime(intChan, primeChan, exitChan)
+	}
 
 	for i := 0; i < cap(exitChan); i++ {
 		<-exitChan
 	}
-	close(exitChan)
+	close(primeChan)
+	for {
+		num, ok := <-primeChan
+		if !ok {
+			break
+		}
+		fmt.Printf("素数: %d\n", num)
+	}
 
 	fmt.Println("退出主线程............")
 }
 
-func writeData() {
-	for i := 1; i <= 50; i++ {
+// 只往intChan中写入数据
+func inputNum(intChan chan<- int) {
+	defer close(intChan)
+	for i := 1; i <= 8000; i++ {
 		intChan <- i
 	}
-	close(intChan)
 }
 
-func readData() {
+// 判断从intChan中取出的是否为素数
+func judgePrime(intChan <-chan int, primeChan chan<- int, exitChan chan<- bool) {
 	for {
-		val, ok := <-intChan
+		num, ok := <-intChan
 		if !ok {
 			break
 		}
-		fmt.Printf("读取管道中数据：%d\n", val)
+		var isPrime = true // 默认为素数
+		for i := 2; i < num; i++ {
+			if num%i == 0 {
+				isPrime = false
+				break
+			}
+		}
+
+		if isPrime {
+			primeChan <- num
+		}
 	}
+
 	exitChan <- true
 }
